489d8d10fca3d50df12d665fc6462c6033453756
Roy Cruz <rfcruz@wisc.edu>
2025-10-03 00:09:16 -0500

    Fix ex1 to swap arrs, not ints

--- assignment1/exercise1.cc
+++ assignment1/exercise1.cc
@@ -1,19 +1,38 @@
 #include <iostream>
 
 void swap_vals(int &a, int &b) {
-    // Swap the values of a and b
     int temp = a;
     a = b;
     b = temp;
 }
 
-int main() {
-    int x = 42;
-    int y = 2025;
-    std::cout << "Values before swap: x = " << x << ", y = " << y << std::endl;
+void print_array(const int* arr, int length) {
+    for (int i = 0; i < length; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
-    swap_vals(x, y);
-    std::cout << "Values after swap: x = " << x << ", y = " << y << std::endl;
+int main() {
+    const int length = 10;
+    int A[length] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int B[length] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+    std::cout << "Array A before swapping: ";
+    print_array(A, length);
+    std::cout << "Array B before swapping: ";
+    print_array(B, length);
+    std::cout << std::endl;
+
+    for (int i = 0; i < length; i++) {
+        swap_vals(A[i], B[i]);
+    }
+
+    std::cout << "Array A after swapping: ";
+    print_array(A, length);
+    std::cout << "Array B after swapping: ";
+    print_array(B, length);
+    std::cout << std::endl;
 
     return 0;
 }
