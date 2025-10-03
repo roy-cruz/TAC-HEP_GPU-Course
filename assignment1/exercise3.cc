61fef3c67b39fa2bda0246e3d4333e3699fadb89
Roy Cruz <rfcruz@wisc.edu>
2025-10-03 13:48:28 -0500

    ex3: play_rps now returns int

--- assignment1/exercise3.cc
+++ assignment1/exercise3.cc
@@ -12,19 +12,20 @@ int input_rps() {
 };
 
 int play_rps(int plyr1, int plyr2) {
+    // Should return 0 if draw, 1 if player 1 wins, 2 if player 2 wins
     plyr1 = plyr1 % 3;
     plyr2 = plyr2 % 3;
 
     if ((plyr1 - plyr2) == 0){
-        std::cout << "Draw!" << std::endl;
+        // Draw
         return 0;
     }
     if (((plyr1 + 1) % 3) == plyr2){
-        std::cout << "Player 1 wins!" << std::endl;
-        return 0;
+        // Player 1 wins
+        return 1;
     }
-    std::cout << "Player 2 wins!" << std::endl;
-    return 0;
+    // Player 2 wins
+    return 2;
 }
 
 int main() {
@@ -35,7 +36,16 @@ int main() {
 
     std::cout << "---------------------" << std::endl;
     std::cout << "Result:" << std::endl;
-    play_rps(input1, input2);
+    int rslt = play_rps(input1, input2);
+
+    // Print the result
+    if (rslt == 0) {
+        std::cout << "It's a draw!" << std::endl;
+    } else if (rslt == 1) {
+        std::cout << "Player 1 wins!" << std::endl; 
+    } else {
+        std::cout << "Player 2 wins!" << std::endl;
+    }
 
     return 0;
 }
